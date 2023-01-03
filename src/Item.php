7d3999f7ddad6e5befc19a05efcfc77d23a1ea9f
MAlexGG <73828751+MAlexGG@users.noreply.github.com>
2023-01-03 19:51:40 +0100

    Update Item.php

--- src/Item.php
+++ src/Item.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-
-//Crear una clase Item que tenga 3 propiedades: name, price y quantity. Cada propiedad debe ser capaz de ser retornada con un método propio.
+//Ejercicio 1 -> Crear una clase Item que tenga 3 propiedades: name, price y quantity cuyo valor será declarado al crear un objeto. Cada propiedad debe ser capaz de ser retornada con un método propio.
+//Exercise 1 -> Create an Item class that has 3 properties: name, price and quantity whose value will be declared when creating an object. Each property must be able to be returned with its own method.
 
 //Escribe tu código aquí...
